8a54de28532549bc9d814bb9f095a5cb1bc7c5e3
maxime <maximelouis321@gmail.com>
2025-10-13 12:02:01 +0200

    Removed obsolete DCLGEN files for CLIENT, COMPTE, and OPERATION; updated FILDB.cbl to reflect changes in file names and structures for CLIENT and COMPTE. Adjusted COBOL declarations and added error handling for file operations.

--- PROGRAMME/FILDB.cbl
+++ PROGRAMME/FILDB.cbl
@@ -14,10 +14,10 @@
            ACCESS MODE IS SEQUENTIAL
            FILE STATUS IS FS-FCLI.
 
-           SELECT FCOMPT ASSIGN TO INP002
+           SELECT FCOMPTE ASSIGN TO INP002
            ORGANIZATION IS SEQUENTIAL
            ACCESS MODE IS SEQUENTIAL
-           FILE STATUS IS FS-FCOMPT.
+           FILE STATUS IS FS-FCOMPTE.
 
        
        DATA DIVISION.
@@ -27,31 +27,38 @@
        FD  FCLI
            RECORD CONTAINS 50 CHARACTERS.
        01  BUFCLIENT.
-          05 BUF-ID-CLIENT           PIC 9(10).
-           05 BUF-NOM-CLIENT         PIC X(10).
-           05 BUF-PRENOM-CLIENT      PIC X(15).
-           05 BUF-ADRESSE-CLIENT     PIC X(15).
+           05 BUF-ID-CLIENT           PIC 9(10).
+           05 BUF-NOM-CLIENT          PIC X(10).
+           05 BUF-PRENOM-CLIENT       PIC X(15).
+           05 BUF-ADRESSE-CLIENT      PIC X(15).
 
-       FD FCOMPT
-           RECORD CONTAINS 50 CHARACTERS.
-       01  BUFCOMPT.
-           05 BUF-ID-COMPT           PIC S9(9) USAGE COMP.
-           05 BUF-ID-CLIENT-COMPT    PIC S9(9) USAGE COMP.
-           05 BUF-CODE-CB            PIC S9(9) USAGE COMP.
-           05 BUF-SOLDE              PIC S9(8)V9(2) USAGE COMP-3.
+       FD FCOMPTE
+           RECORD CONTAINS 34 CHARACTERS.
+       01  BUFCOMPTE.
+      *    BUFFER EN FORMAT TEXTE POUR LECTURE FICHIER SEQUENTIEL
+           05 BUF-ID-COMPTE            PIC 9(10).
+           05 BUF-ID-CLIENT-COMPTE     PIC 9(10).
+           05 BUF-CODE-CB              PIC 9(4).
+           05 BUF-SOLDE                PIC X(10).
 
  
        WORKING-STORAGE SECTION.
        
        77  FS-FCLI PIC X(2).
-       77  FS-FCOMPT PIC X(2).
-           
+       77  FS-FCOMPTE PIC X(2).
+
+       01  TEMP-SOLDE                PIC 9(10).
+
            EXEC SQL
               INCLUDE SQLCA
            END-EXEC.
        
            EXEC SQL
               INCLUDE CLIENT
+           END-EXEC.
+           
+           EXEC SQL
+              INCLUDE COMPTE
            END-EXEC.
            
        01  ERR-MSG.
@@ -74,16 +81,31 @@
 
            PERFORM 6000-OPEN-CLI
               THRU 6000-OPEN-CLI-EXIT.
+           
+           PERFORM 6000-OPEN-COMPTE
+              THRU 6000-OPEN-COMPTE-EXIT.
 
            PERFORM 6110-READ-CLI
               THRU 6110-READ-CLI-EXIT.
-           
+
+           PERFORM 6110-READ-COMPTE
+              THRU 6110-READ-COMPTE-EXIT.
+
+
            PERFORM 1000-TRAITER-CLI
               THRU 1000-TRAITER-CLI-EXIT
               UNTIL FS-FCLI = '10'.
-          
+
+           PERFORM 1000-TRAITER-COMPTE
+              THRU 1000-TRAITER-COMPTE-EXIT
+              UNTIL FS-FCOMPTE = '10'.
+
+
            PERFORM 6220-CLOSE-CLI
               THRU 6220-CLOSE-CLI-EXIT.
+
+           PERFORM 6220-CLOSE-COMPTE
+              THRU 6220-CLOSE-COMPTE-EXIT.
 
            PERFORM 9999-FIN-PROGRAMME-DEB
               THRU 9999-FIN-PROGRAMME-FIN.
@@ -103,6 +125,16 @@
        6000-OPEN-CLI-EXIT.
            EXIT.
 
+       6000-OPEN-COMPTE.
+           OPEN INPUT FCOMPTE.
+           IF FS-FCOMPTE NOT = '00'
+               DISPLAY 'ERROR OPENING FCOMPT: '
+               DISPLAY 'VALEUR DU FILE STATUS: ' FS-FCOMPTE
+               GO TO 9999-ERREUR-PROGRAMME-DEB
+           END-IF.
+       6000-OPEN-COMPTE-EXIT.
+           EXIT.
+
 
        6110-READ-CLI.
            READ FCLI
@@ -113,22 +145,29 @@
            END-IF.
        6110-READ-CLI-EXIT.
            EXIT.
-           
+
+       6110-READ-COMPTE.
+           READ FCOMPTE
+           IF FS-FCOMPTE NOT = '00' AND NOT = '10'
+               DISPLAY 'ERROR READING FCOMPT: '
+               DISPLAY 'VALEUR DU FILE STATUS: ' FS-FCOMPTE
+               GO TO 9999-ERREUR-PROGRAMME-DEB
+           END-IF.
+       6110-READ-COMPTE-EXIT.
+           EXIT.
+
+
        1000-TRAITER-CLI.
-           MOVE BUF-ID-CLIENT TO WS-ID-CLIENT.
+           MOVE BUF-ID-CLIENT TO WS-ID-CLIENT OF DCLCLIENT.
            MOVE BUF-NOM-CLIENT TO WS-NOM-CLIENT.
            MOVE BUF-PRENOM-CLIENT TO WS-PRENOM-CLIENT.
            MOVE BUF-ADRESSE-CLIENT TO WS-ADRESSE-CLIENT.
 
            EXEC SQL
-              WHENEVER SQLERROR CONTINUE
-           END-EXEC.
-
-           EXEC SQL
-             INSERT INTO API8.CLIENT 
+            INSERT INTO API8.CLIENT 
            (ID_CLIENT, NOM_CLIENT, PRENOM_CLIENT, ADRESSE_CLIENT) VALUES
-           (:WS-ID-CLIENT, :WS-NOM-CLIENT, :WS-PRENOM-CLIENT, 
-                                                    :WS-ADRESSE-CLIENT)
+           (:DCLCLIENT.WS-ID-CLIENT, :WS-NOM-CLIENT, 
+           :WS-PRENOM-CLIENT, :WS-ADRESSE-CLIENT)
            END-EXEC.
 
 
@@ -142,6 +181,44 @@
        1000-TRAITER-CLI-EXIT.
            EXIT.
 
+       1000-TRAITER-COMPTE.
+           
+           MOVE BUF-ID-COMPTE TO WS-ID-COMPTE.
+           MOVE BUF-ID-CLIENT-COMPTE TO WS-ID-CLIENT OF DCLCOMPTE.
+           MOVE BUF-CODE-CB TO WS-CODE-CB.
+           MOVE BUF-SOLDE TO TEMP-SOLDE.
+           MOVE TEMP-SOLDE TO WS-SOLDE.
+
+
+           EXEC SQL
+            INSERT INTO API8.COMPTE
+           (ID_COMPTE, ID_CLIENT, CODE_CB, SOLDE) VALUES
+           (:WS-ID-COMPTE, :DCLCOMPTE.WS-ID-CLIENT, 
+           :WS-CODE-CB, :WS-SOLDE)
+           END-EXEC.
+
+
+           EXEC SQL
+              WHENEVER SQLERROR GOTO 9998-ERROR-DB2
+           END-EXEC.
+
+           PERFORM 6110-READ-COMPTE
+              THRU 6110-READ-COMPTE-EXIT.
+       
+       1000-TRAITER-COMPTE-EXIT.
+           EXIT.
+
+       6220-CLOSE-COMPTE.
+           CLOSE FCOMPTE.
+           IF FS-FCOMPTE NOT = '00'
+               DISPLAY 'ERROR CLOSING FCOMPT: '
+               DISPLAY 'VALEUR DU FILE STATUS: ' FS-FCOMPTE
+               GO TO 9999-ERREUR-PROGRAMME-DEB
+           END-IF.
+       6220-CLOSE-COMPTE-EXIT.
+           EXIT.
+
+
        6220-CLOSE-CLI.
            CLOSE FCLI.
            IF FS-FCLI NOT = '00'
@@ -151,7 +228,7 @@
            END-IF.
        6220-CLOSE-CLI-EXIT.
            EXIT.
-
+       
        
        9998-ERROR-DB2.
             DISPLAY 'ERREUR DB2 '.
